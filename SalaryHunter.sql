da9cc62388eb606c74edd6b4ef78d06f0d89b531
romine24 <northeastern.edu>
2024-11-28 13:48:09 -0500

    Insert initial data.

--- SalaryHunter.sql
+++ SalaryHunter.sql
@@ -1,9 +1,7 @@
 DROP DATABASE IF EXISTS SalaryHunter;
-
 CREATE DATABASE IF NOT EXISTS SalaryHunter;
 USE SalaryHunter;
 
--- DROP TABLE IF EXISTS Registered_User ;
 CREATE TABLE IF NOT EXISTS Registered_User (
 	username VARCHAR(64) PRIMARY KEY,
     password VARCHAR(64) NOT NULL
@@ -25,7 +23,8 @@ CREATE TABLE IF NOT EXISTS Company_Branch (
 );
 
 CREATE TABLE IF NOT EXISTS Job_Position (
-	position_name VARCHAR(64) PRIMARY KEY,
+    job_id INT AUTO_INCREMENT PRIMARY KEY,
+	position_name VARCHAR(64),
     description VARCHAR(500),
     year YEAR NOT NULL,
     salary_amount DECIMAL(10, 2) NOT NULL CHECK (salary_amount >= 0),
@@ -36,9 +35,15 @@ CREATE TABLE IF NOT EXISTS Job_Position (
 
 CREATE TABLE IF NOT EXISTS Benifit (
     benefit_name VARCHAR(64) PRIMARY KEY,
-	benefit_type ENUM('Insurance', 'Holiday', 'Stock', 'Retirement', 'Family'),
-    position_name VARCHAR(64),
-    FOREIGN KEY (position_name) REFERENCES Job_Position(position_name)
+	benefit_type ENUM('Insurance', 'Holiday', 'Stock', 'Retirement', 'Family')
+);
+
+CREATE TABLE IF NOT EXISTS Job_Benefit (
+	job_id INT,
+    benefit_name VARCHAR(64),
+    FOREIGN KEY (job_id) REFERENCES Job_Position(job_id)
+		ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (benefit_name) REFERENCES Benifit(benefit_name)
 		ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -46,10 +51,10 @@ CREATE TABLE IF NOT EXISTS Background (
 	background_id INT AUTO_INCREMENT PRIMARY KEY,
 	degree_level ENUM('BS', 'MS', 'PhD'),
     university_name VARCHAR(64) NOT NULL,
-    year_of_work VARCHAR(2) NOT NULL
+    year_of_work INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS SKill (
+CREATE TABLE IF NOT EXISTS Skill (
 	skill_name VARCHAR(64) PRIMARY KEY
 );
 
@@ -67,7 +72,7 @@ CREATE TABLE IF NOT EXISTS User_Skill (
     skill_name VARCHAR(64) NOT NULL,
     FOREIGN KEY (username) REFERENCES Registered_User(username)
 		ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (skill_name) REFERENCES SKill(skill_name)
+	FOREIGN KEY (skill_name) REFERENCES Skill(skill_name)
 		ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -84,74 +89,140 @@ CREATE TABLE IF NOT EXISTS Interview (
 CREATE TABLE IF NOT EXISTS User_Interview_Position (
 	username VARCHAR(64) NOT NULL,
     interview_id INT NOT NULL,
-    position_name VARCHAR(64) NOT NULL,
-    has_exprience BOOLEAN NOT NULL DEFAULT FALSE,
+    job_id INT NOT NULL,
+    has_interview BOOLEAN NOT NULL DEFAULT FALSE,
     got_position BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (username) REFERENCES Registered_User(username),
     FOREIGN KEY (interview_id) REFERENCES Interview(interview_id),
-    FOREIGN KEY (position_name) REFERENCES Job_Position(position_name)
+    FOREIGN KEY (job_id) REFERENCES Job_Position(job_id)
 );
 
-INSERT INTO Registered_User (username, password) VALUES('abc', '123456');
+INSERT INTO Registered_User (username, password)
+VALUES 
+    ('a', '123'),
+    ('b', '456'),
+    ('c', '789');
 
 INSERT INTO State_Area (state_abbr, state_name, in_area) 
 VALUES 
-    ('AL', 'Alabama', 'south'),
-    ('AK', 'Alaska', 'west'),
-    ('AZ', 'Arizona', 'west'),
-    ('AR', 'Arkansas', 'south'),
-    ('CA', 'California', 'west'),
-    ('CO', 'Colorado', 'west'),
-    ('CT', 'Connecticut', 'northeast'),
-    ('DE', 'Delaware', 'south'),
-    ('FL', 'Florida', 'south'),
-    ('GA', 'Georgia', 'south'),
-    ('HI', 'Hawaii', 'west'),
-    ('ID', 'Idaho', 'west'),
-    ('IL', 'Illinois', 'midwest'),
-    ('IN', 'Indiana', 'midwest'),
-    ('IA', 'Iowa', 'midwest'),
-    ('KS', 'Kansas', 'midwest'),
-    ('KY', 'Kentucky', 'south'),
-    ('LA', 'Louisiana', 'south'),
-    ('ME', 'Maine', 'northeast'),
-    ('MD', 'Maryland', 'south'),
-    ('MA', 'Massachusetts', 'northeast'),
-    ('MI', 'Michigan', 'midwest'),
-    ('MN', 'Minnesota', 'midwest'),
-    ('MS', 'Mississippi', 'south'),
-    ('MO', 'Missouri', 'midwest'),
-    ('MT', 'Montana', 'west'),
-    ('NE', 'Nebraska', 'midwest'),
-    ('NV', 'Nevada', 'west'),
-    ('NH', 'New Hampshire', 'northeast'),
-    ('NJ', 'New Jersey', 'northeast'),
-    ('NM', 'New Mexico', 'west'),
-    ('NY', 'New York', 'northeast'),
-    ('NC', 'North Carolina', 'south'),
-    ('ND', 'North Dakota', 'midwest'),
-    ('OH', 'Ohio', 'midwest'),
-    ('OK', 'Oklahoma', 'south'),
-    ('OR', 'Oregon', 'west'),
-    ('PA', 'Pennsylvania', 'northeast'),
-    ('RI', 'Rhode Island', 'northeast'),
-    ('SC', 'South Carolina', 'south'),
-    ('SD', 'South Dakota', 'midwest'),
-    ('TN', 'Tennessee', 'south'),
-    ('TX', 'Texas', 'south'),
-    ('UT', 'Utah', 'west'),
-    ('VT', 'Vermont', 'northeast'),
-    ('VA', 'Virginia', 'south'),
-    ('WA', 'Washington', 'west'),
-    ('WV', 'West Virginia', 'south'),
-    ('WI', 'Wisconsin', 'midwest'),
-    ('WY', 'Wyoming', 'west');
+    ('AL', 'Alabama', 'South'),
+    ('AK', 'Alaska', 'West'),
+    ('AZ', 'Arizona', 'West'),
+    ('AR', 'Arkansas', 'South'),
+    ('CA', 'California', 'West'),
+    ('CO', 'Colorado', 'West'),
+    ('CT', 'Connecticut', 'Northeast'),
+    ('DE', 'Delaware', 'South'),
+    ('FL', 'Florida', 'South'),
+    ('GA', 'Georgia', 'South'),
+    ('HI', 'Hawaii', 'West'),
+    ('ID', 'Idaho', 'West'),
+    ('IL', 'Illinois', 'MidWest'),
+    ('IN', 'Indiana', 'Midwest'),
+    ('IA', 'Iowa', 'Midwest'),
+    ('KS', 'Kansas', 'Midwest'),
+    ('KY', 'Kentucky', 'South'),
+    ('LA', 'Louisiana', 'South'),
+    ('ME', 'Maine', 'Northeast'),
+    ('MD', 'Maryland', 'South'),
+    ('MA', 'Massachusetts', 'Northeast'),
+    ('MI', 'Michigan', 'Midwest'),
+    ('MN', 'Minnesota', 'Midwest'),
+    ('MS', 'Mississippi', 'South'),
+    ('MO', 'Missouri', 'Midwest'),
+    ('MT', 'Montana', 'West'),
+    ('NE', 'Nebraska', 'Midwest'),
+    ('NV', 'Nevada', 'West'),
+    ('NH', 'New Hampshire', 'Northeast'),
+    ('NJ', 'New Jersey', 'Northeast'),
+    ('NM', 'New Mexico', 'West'),
+    ('NY', 'New York', 'Northeast'),
+    ('NC', 'North Carolina', 'South'),
+    ('ND', 'North Dakota', 'Midwest'),
+    ('OH', 'Ohio', 'Midwest'),
+    ('OK', 'Oklahoma', 'South'),
+    ('OR', 'Oregon', 'West'),
+    ('PA', 'Pennsylvania', 'Northeast'),
+    ('RI', 'Rhode Island', 'Northeast'),
+    ('SC', 'South Carolina', 'South'),
+    ('SD', 'South Dakota', 'Midwest'),
+    ('TN', 'Tennessee', 'South'),
+    ('TX', 'Texas', 'South'),
+    ('UT', 'Utah', 'West'),
+    ('VT', 'Vermont', 'Northeast'),
+    ('VA', 'Virginia', 'South'),
+    ('WA', 'Washington', 'West'),
+    ('WV', 'West Virginia', 'South'),
+    ('WI', 'Wisconsin', 'Midwest'),
+    ('WY', 'Wyoming', 'West');
 
-INSERT INTO Job_Position (position_name, description, year, salary_amount) 
-	VALUES('SoftWare Engineer 1', 'software develop.', 2023, 100000.00);
+INSERT INTO Company_Branch (company_name, state_abbr, industry_name)
+VALUES
+    ('Z', 'MA', 'Technology'),
+    ('Y', 'NY', 'AI'),
+    ('X', 'CA', 'HealthCare');
 
+INSERT INTO Job_Position (position_name, description, year, salary_amount, company_id)
+VALUES 
+    ('Software Engineer I', 'Develop applications', 2024, 120000.00, 1),
+	('Software Engineer III', 'Design AI systems', 2024, 500000.00, 2),
+	('Data Analyst', 'Analyze medical data', 2024, 120000.00, 3);
+                 
+INSERT INTO Benifit (benefit_name, benefit_type)
+VALUES 
+    ('401K', 'Retirement'),
+	('Share Incentive Plan', 'Stock');
 
--- DROP FUNCTION IF EXISTS check_user_exist;
+INSERT INTO Job_Benefit (job_id, benefit_name)
+VALUES
+    (1, '401K' ),
+	(2, '401K'),
+    (3, '401K'),
+    (1, 'Share Incentive Plan');
+
+INSERT INTO Background (degree_level, university_name, year_of_work)
+VALUES 
+    ('MS', 'Northeastern University', 1),
+    ('PhD', 'Harvard University', 8),
+    ('BS', 'Northeastern University', 0);
+
+INSERT INTO User_Background (username, background_id) 
+VALUES
+    ('a', 3),
+	('a', 1),
+    ('b', 2),
+    ('c', 3);
+
+INSERT INTO Skill (skill_name)
+VALUES 
+    ('Python'),
+    ('SQL'),
+    ('R'),
+    ('Machine Learning');
+
+INSERT INTO User_Skill (username, skill_name)
+VALUES 
+    ('a', 'Python'),
+    ('a', 'SQL'),
+	('b', 'SQL'),
+	('b', 'Python'),
+    ('b', 'Machine Learning'),
+	('c', 'R');
+
+INSERT INTO Interview (interview_type, description)
+VALUES 
+    ('Technical Interview', 'Coding and algorithm problems'),
+	('Technical Interview', 'Complex machine learning problems'),
+	('Behavioral Interview', 'Assess behavioral skills and cultural fit');
+
+INSERT INTO User_Interview_Position (username, interview_id, job_id, has_interview, got_position)
+VALUES 
+    ('a', 1, 1, TRUE, TRUE),
+	('c', 3, 3, TRUE, TRUE),
+    ('a', 3, 1, TRUE, FALSE),
+	('b', 2, 2, TRUE, TRUE);
+
+-- check if username exists in the database
 DELIMITER //
 CREATE FUNCTION check_user_exist(
 	user VARCHAR(64) 
@@ -165,6 +236,7 @@ BEGIN
 END //
 DELIMITER ;
 
+-- check if password is matched
 DELIMITER //
 CREATE FUNCTION password_match(
     user VARCHAR(64) 
@@ -178,6 +250,7 @@ BEGIN
 END //
 DELIMITER ;
 
+-- register new user
 DELIMITER //
 CREATE PROCEDURE InsertUser(
     IN p_username VARCHAR(64),
@@ -189,26 +262,25 @@ BEGIN
 END //
 DELIMITER ;
 
-DELIMITER //
-CREATE PROCEDURE user_record(
-    IN r_username VARCHAR(64)
-)
-BEGIN
-    SELECT sa.in_area, cb.industry_name, cb.company_name, jp.position_name, jp.description, 
-           jp.year, jp.salary_amount, be.benefit_type, be.benfit_name, ba.degree_level, 
-           ba.year_of_work, sk.skill_name, iv.type, iv.description FROM Registered_User
-        INNER JOIN User_Interview_Position USING username
-        INNER JOIN Interview AS iv USING interview_id
-        INNER JOIN Job_Position AS jp USING position_name
-        INNER JOIN Company_Branch AS cb USING company_id
-        INNER JOIN State_Area AS sa USING state_abbr
-        INNER JOIN Benefit AS be USING position_name
-        INNER JOIN User_Background USING username
-        INNER JOIN Background AS ba USING background_id
-        INNER JOIN User_Skill USING username
-        INNER JOIN Skill AS sk USING skill_name
-    WHERE username = r_username;
-END //
-DELIMITER ;
-
-
+-- display user record
+-- DELIMITER //
+-- CREATE PROCEDURE user_record(
+--     IN r_username VARCHAR(64)
+-- )
+-- BEGIN
+--     SELECT sa.in_area, cb.industry_name, cb.company_name, jp.position_name, jp.description, 
+--            jp.year, jp.salary_amount, be.benefit_type, be.benefit_name, ba.degree_level, 
+--            ba.year_of_work, sk.skill_name, iv.type, iv.description FROM Registered_User
+--         INNER JOIN User_Interview_Position USING username
+--         INNER JOIN Interview AS iv USING interview_id
+--         INNER JOIN Job_Position AS jp USING position_name
+--         INNER JOIN Company_Branch AS cb USING company_id
+--         INNER JOIN State_Area AS sa USING state_abbr
+--         INNER JOIN Benefit AS be USING position_name
+--         INNER JOIN User_Background USING username
+--         INNER JOIN Background AS ba USING background_id
+--         INNER JOIN User_Skill USING username
+--         INNER JOIN Skill AS sk USING skill_name
+--     WHERE username = r_username;
+-- END //
+-- DELIMITER ;
